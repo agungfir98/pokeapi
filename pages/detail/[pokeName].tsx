b8c0f74966375e9bf1a851686f2c23fa8c0883ed
Agung Firmansyah <agungfir98@gmail.com>
2022-02-11 23:37:27 +0700

    tweaking detail page

--- pages/detail/[pokeName].tsx
+++ pages/detail/[pokeName].tsx
@@ -1,4 +1,3 @@
-import { Tema } from "../../switch/switch";
 import Image from "next/image";
 import { GET_DETAIL } from "../../graphql/graphql";
 import { useRouter } from "next/router";
@@ -18,6 +17,57 @@ export default function Detail() {
 
   const { name, stats, sprites, types } = data.pokemon;
   const move = data.pokemon.moves;
+
+  const Tema = (tipe: string): string => {
+    switch (tipe) {
+      case "normal":
+        return "bg-gradient-to-r from-gray-500 to-gray-500 text-white";
+        break;
+      case "fighting":
+      case "dragon":
+        return "bg-gradient-to-r from-red-500 to-yellow-500 text-white";
+        break;
+      case "poison":
+      case "psychic":
+        return "poison bg-gradient-to-r from-violet-900 to-violet-500 text-white";
+        break;
+      case "ground":
+      case "rock":
+        return "bg-gradient-to-r from-yellow-900 to-orange-900 text-white";
+        break;
+      case "ghost":
+      case "dark":
+      case "shadow":
+        return "bg-gradient-to-r from-gray-700 to-gray-800 text-white";
+        break;
+      case "steel":
+        return "bg-gradient-to-r from-gray-800 to-yellow-900 text-white";
+        break;
+      case "fire":
+        return "bg-gradient-to-u from-red-500 to-red-300";
+        break;
+      case "flying":
+      case "water":
+      case "ice":
+        return "bg-gradient-to-r from-cyan-500 to-blue-500 text-white";
+        break;
+      case "grass":
+      case "bug":
+        return "bg-gradient-to-r from-green-700 to-green-500 text-white";
+        break;
+      case "electric":
+        return "bg-gradient-to-r from-yellow-600 to-yellow-400 text-white";
+        break;
+      case "fairy":
+        return "bg-gradient-to-r from-cyan-500 to-blue-500";
+        break;
+      case "unknown":
+        return "bg-gradient-to-r from-cyan-500 to-blue-500";
+        break;
+      default:
+        return "bg-yellow-900 -text-white";
+    }
+  };
 
   function bgPokeType() {
     return types.map((i: tipe) => {
