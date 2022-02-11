88e724231978fe0053d9f6ffe91340e2dc3ad7bf
Agung Firmansyah <agungfir98@gmail.com>
2022-02-11 23:57:53 +0700

    tweaking

--- pages/detail/[pokeName].tsx
+++ pages/detail/[pokeName].tsx
@@ -38,13 +38,14 @@ export default function Detail() {
       case "ghost":
       case "dark":
       case "shadow":
+      case "unknown":
         return "bg-gradient-to-r from-gray-700 to-gray-800 text-white";
         break;
       case "steel":
         return "bg-gradient-to-r from-gray-800 to-yellow-900 text-white";
         break;
       case "fire":
-        return "bg-gradient-to-u from-red-500 to-red-300";
+        return "bg-gradient-to-r from-red-500 to-red-300 text-white";
         break;
       case "flying":
       case "water":
@@ -53,16 +54,11 @@ export default function Detail() {
         break;
       case "grass":
       case "bug":
+      case "fairy":
         return "bg-gradient-to-r from-green-700 to-green-500 text-white";
         break;
       case "electric":
         return "bg-gradient-to-r from-yellow-600 to-yellow-400 text-white";
-        break;
-      case "fairy":
-        return "bg-gradient-to-r from-cyan-500 to-blue-500";
-        break;
-      case "unknown":
-        return "bg-gradient-to-r from-cyan-500 to-blue-500";
         break;
       default:
         return "bg-yellow-900 -text-white";
